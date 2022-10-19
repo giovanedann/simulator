37a9e0f44c1eee7161ef3a34aa75936a4d2fb175
Giovane <giovanedaniell@gmail.com>
2022-10-19 14:00:42 -0300

    refactor: create a consumer on the main package

--- main.go
+++ main.go
@@ -2,16 +2,29 @@ package main
 
 import (
 	"fmt"
-	route2 "github.com/giovanedann/simulator/app/route"
+	"log"
+	kafka2 "github.com/giovanedann/simulator/app/kafka"
+	"github.com/giovanedann/simulator/infra/kafka"
+	ckafka "github.com/confluentinc/confluent-kafka-go/kafka"
+	"github.com/joho/godotenv"
 )
 
-func main() {
-	route := route2.Route{
-		ID: "1",
-		ClientID: "1",
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("error loading .env file")
 	}
+}
 
-	route.LoadPositions()
-	stringjson, _ := route.ExportJsonPositions()
-	fmt.Println(stringjson[0])
+func main() {
+	msgChannel := make(chan *ckafka.Message) // creating a channel to receive the messages
+	consumer := kafka.NewKafkaConsumer(msgChannel) // creating a consumer to watch over the channel
+
+	go consumer.Consume() // running the consumer in a sepparated thread
+
+	// looping over the msgChannel to print the messages every time a new message arrives
+	for msg := range msgChannel { 
+		fmt.Println(string(msg.Value))
+		go kafka2.Produce(msg)
+	}
 }

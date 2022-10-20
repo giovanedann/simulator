e885e08b48038c921c6e0a2878d741f67b63b512
Giovane <giovanedaniell@gmail.com>
2022-10-20 12:27:46 -0300

    feat: create kafka config for route model

--- api/src/routes/routes.module.ts
+++ api/src/routes/routes.module.ts
@@ -1,5 +1,6 @@
 import { MongooseModule } from '@nestjs/mongoose';
 import { Module } from '@nestjs/common';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 import { RoutesService } from './routes.service';
 import { RoutesController } from './routes.controller';
 import { Route, RouteSchema } from './entities/route.entity';
@@ -7,6 +8,25 @@ import { Route, RouteSchema } from './entities/route.entity';
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Route.name, schema: RouteSchema }]),
+    ClientsModule.register([
+      {
+        name: 'KAFKA_SERVICE',
+        transport: Transport.KAFKA,
+        options: {
+          client: {
+            clientId: process.env.KAFKA_CLIENT_ID,
+            brokers: [process.env.KAFKA_BROKER],
+          },
+          consumer: {
+            groupId:
+              !process.env.KAFMA_CONSUMER_GROUP_ID ||
+              process.env.KAFKA_CONSUMER_GROUP_ID === ''
+                ? 'my-consumer-' + Math.random()
+                : process.env.KAFKA_CONSUMER_GROUP_ID,
+          },
+        },
+      },
+    ]),
   ],
   controllers: [RoutesController],
   providers: [RoutesService],

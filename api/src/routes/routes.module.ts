3af5e1125d2c4b94d5eb491c1b124fbc959175bd
Giovane <giovanedaniell@gmail.com>
2022-10-20 12:46:40 -0300

    refactor: change route module to use Async register function

--- api/src/routes/routes.module.ts
+++ api/src/routes/routes.module.ts
@@ -8,23 +8,25 @@ import { Route, RouteSchema } from './entities/route.entity';
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Route.name, schema: RouteSchema }]),
-    ClientsModule.register([
+    ClientsModule.registerAsync([
       {
         name: 'KAFKA_SERVICE',
-        transport: Transport.KAFKA,
-        options: {
-          client: {
-            clientId: process.env.KAFKA_CLIENT_ID,
-            brokers: [process.env.KAFKA_BROKER],
+        useFactory: () => ({
+          transport: Transport.KAFKA,
+          options: {
+            client: {
+              clientId: process.env.KAFKA_CLIENT_ID,
+              brokers: [process.env.KAFKA_BROKER],
+            },
+            consumer: {
+              groupId:
+                !process.env.KAFMA_CONSUMER_GROUP_ID ||
+                process.env.KAFKA_CONSUMER_GROUP_ID === ''
+                  ? 'my-consumer-' + Math.random()
+                  : process.env.KAFKA_CONSUMER_GROUP_ID,
+            },
           },
-          consumer: {
-            groupId:
-              !process.env.KAFMA_CONSUMER_GROUP_ID ||
-              process.env.KAFKA_CONSUMER_GROUP_ID === ''
-                ? 'my-consumer-' + Math.random()
-                : process.env.KAFKA_CONSUMER_GROUP_ID,
-          },
-        },
+        }),
       },
     ]),
   ],

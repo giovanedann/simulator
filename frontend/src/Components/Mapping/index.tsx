4579cb40cb7d7bca39b22005bc5ed09e099c776c
Giovane <giovanedaniell@gmail.com>
2022-10-21 12:03:19 -0300

    [frontend] refactor: using Map util class on Mapping component
    also using a custom color on the marker icons

--- frontend/src/Components/Mapping/index.tsx
+++ frontend/src/Components/Mapping/index.tsx
@@ -1,12 +1,38 @@
 import { Grid, Select, MenuItem, Button } from '@material-ui/core'
-import { ChangeEvent, FormEvent, useEffect, useState, useCallback } from 'react'
+import { Loader } from 'google-maps'
+import {
+  ChangeEvent,
+  FormEvent,
+  useEffect,
+  useState,
+  useCallback,
+  useRef
+} from 'react'
 import { Route } from '../../interfaces/Route'
+import { getCurrentPosition } from '../../utils/geolocation'
+import { makeCarIcon, makeMarkerIcon, Map } from '../../utils/map'
 
 const API_URL = import.meta.env.VITE_API_URL
+
+const mapLoader = new Loader(import.meta.env.VITE_GOOGLE_API_KEY)
+
+const colors = [
+  "#b71c1c",
+  "#4a148c",
+  "#2e7d32",
+  "#e65100",
+  "#2962ff",
+  "#c2185b",
+  "#FFCD00",
+  "#3e2723",
+  "#03a9f4",
+  "#827717",
+];
 
 function Mapping() {
   const [routes, setRoutes] = useState<Route[]>([])
   const [selectedRouteId, setSelectedRouteId] = useState<string>('')
+  const mapRef = useRef<Map>()
 
   useEffect(() => {
     fetch(`${API_URL}/routes`)
@@ -14,17 +40,52 @@ function Mapping() {
       .then((data) => setRoutes(data))
   }, [])
 
-  function handleRouteChange(event: ChangeEvent<{ name?: string; value: unknown }>) {
+  useEffect(() => {
+    ;(async () => {
+      const [, position] = await Promise.all([
+        mapLoader.load(),
+        getCurrentPosition({ enableHighAccuracy: true })
+      ])
+
+      const mapDiv = document.getElementById('map') as HTMLElement
+
+      mapRef.current = new Map(mapDiv, {
+        zoom: 15,
+        center: position
+      })
+    })()
+  }, [])
+
+  function handleRouteChange(
+    event: ChangeEvent<{ name?: string; value: unknown }>
+  ) {
     setSelectedRouteId(event.target.value as string)
   }
 
-  const startRoute = useCallback((event: FormEvent) => {
-    event.preventDefault();
-    alert(selectedRouteId);
-  }, [selectedRouteId]);
+  const startRoute = useCallback(
+    (event: FormEvent) => {
+      event.preventDefault()
+
+      const route = routes.find((route) => route._id === selectedRouteId)
+      const randomColorIndex = Math.random() * colors.length 
+      const iconColor = colors[Math.floor(randomColorIndex)]
+
+      mapRef.current?.addRoute(selectedRouteId, {
+        currentMarkerOptions: {
+          position: route?.startPosition,
+          icon: makeCarIcon(iconColor)
+        },
+        endMarkerOptions: {
+          position: route?.endPosition,
+          icon: makeMarkerIcon(iconColor)
+        }
+      })
+    },
+    [selectedRouteId]
+  )
 
   return (
-    <Grid container>
+    <Grid container style={{ height: '100%' }}>
       <Grid item xs={12} sm={3}>
         <form onSubmit={startRoute}>
           <Select

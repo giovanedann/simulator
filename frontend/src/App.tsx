ebea1972e3d8590da02a608efe8be8b4cccfc62c
Giovane <giovanedaniell@gmail.com>
2022-10-21 09:33:03 -0300

    feat: add MUI CssBaseline component to change background color

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,11 @@
-import { MuiThemeProvider } from '@material-ui/core'
+import { CssBaseline, MuiThemeProvider } from '@material-ui/core'
 import Mapping from './Components/Mapping'
 import theme from './styles/theme'
 
 function App() {
   return (
     <MuiThemeProvider theme={theme}>
+      <CssBaseline />
       <Mapping />
     </MuiThemeProvider>
   )
